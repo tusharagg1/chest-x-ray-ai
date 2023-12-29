0e299e556d06f9de3108f7e4980a79a5784c0c43
Allison Cook <allison.r.j.cook@gmail.com>
2023-12-29 00:47:03 -0500

    style: improved login page frontend style

--- src/frontend/src/app/page.tsx
+++ src/frontend/src/app/page.tsx
@@ -1,12 +1,16 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState} from 'react';
+import { createStyleRegistry,StyleRegistry } from 'styled-jsx';
 
+//import './styles/login.css';
 import Button from '@/components/buttons/Button';
+import UnderlineLink from '@/components/links/UnderlineLink';
+import NextImage from '@/components/NextImage';
+import Image from 'next/image';
 
-import {getUserData} from './backend/apis';
-
+//import {getUserData} from './backend/apis';
 
 export default function loginPage() {
     const [emailError, setEmailError] = useState(false);
@@ -16,11 +20,6 @@ export default function loginPage() {
     const [password, setPassword] = useState('');
     const [emailErrorMessage, setEmailErrorMessage] = useState('');
     const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
-
-
-    //testing
-    //const [test, settestText] = useState('');
-
     /**
     * @description Validates the email and password, calls the apis
     */
@@ -48,29 +47,38 @@ export default function loginPage() {
             setPasswordError(false);
         }
 
-
-        //settestText("email: " + email + " Password: " + password);
-        
-        
         //TO-DO: call login api
         
     }
 
     return (
-        <main className='bg-indigo-100'>
+        <main className='bg-indigo-100 min-h-screen'>
             <section className='py-2'>
-                <div className='layout relative flex min-h-screen flex-col py-12 items-center justify-center text-center gap-2'>
+            <div className='layout relative flex min-h-screen flex-col items-center justify-center text-center gap-5'>
+                <h1 className='text-indigo-500'>RadiAIdance PACS</h1>
+                <div className='bg-gray-500'>
+                    <Image
+                        //className='flex'
+                        src="/images/loginImage.png"
+                        alt='x-ray results' 
+                        sizes="100vw"
+                        //fill
+                        style={{width: '100%', height: 'auto'}}
+                        width={500}
+                        height={500}
+                    />
+                </div>
+                <div className='bg-white py-5 gap-2' style={{"width" : "40%", "height" : "50%", zIndex: '5'}} >
                     <h2 className='text-indigo-500'>Login</h2>
-
-                    <form method='post' onSubmit={handleSubmit} className='mt-10'>
+                    <p className='text-gray-500'>Sign in to your account</p>
+                    <form method='post' onSubmit={handleSubmit} className='mt-3'>
                         <ol className='space-y-2'>
-                            <div className='bg-white '>
-                            <input type='email' value={email} onChange={e => setEmail(e.target.value)} placeholder='Email'/>
+                            <div>
+                                <input style={{background: 'url("/images/person.png") no-repeat left', paddingLeft: '10%', backgroundColor: 'rgb(229, 231, 235)', width: '65%'}} type='email' value={email} onChange={e => setEmail(e.target.value)} placeholder='Email'/>
                             </div>
-                            <div className='bg-white '>
-                            <input type='text' value={password} onChange={e => setPassword(e.target.value)} placeholder='Password'/>
+                            <div>
+                                <input style={{background: 'url("/images/lock.png") no-repeat left', paddingLeft: '10%', backgroundColor: 'rgb(229, 231, 235)', width:'65%'}} type='text' value={password} onChange={e => setPassword(e.target.value)} placeholder='Password'/>
                             </div>
-
                             <div className='flex text-center justify-center'>
                             <Button
                             size='base'
@@ -78,19 +86,28 @@ export default function loginPage() {
                             isLoading={loading}
                             type='submit'
                             >
-                            Sign In
+                            Login
                             </Button>
                             </div>
                         </ol>
                     </form>
+                    <UnderlineLink
+                    href='/X-RayStudy'
+                    className='text-gray-500 text-sm'
+                    >
+                    Forgot password
+                    </UnderlineLink>
 
                     {emailError && <p className='text-red-500'>{emailErrorMessage}</p>}
                     {passwordError && <p className='text-red-500'>{passwordErrorMessage}</p>}
 
                 </div>
+                <div style={{paddingLeft: '2%', paddingTop: '17%', position: 'absolute', zIndex: '3', "width" : "42%", "height" : "72%"}}>
+                    <div className='bg-indigo-500 ' style={{"width" : "100%", "height" : "100%"}}></div>
+                </div>
+                </div>
             </section>
         </main>
-
-    )
+    );
 
 }
