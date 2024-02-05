cf126afef9f4d9e5ec9d94a1e19a22584a6a5e7b
Nathaniel Hu <57736234+Nathaniel-Hu@users.noreply.github.com>
2024-02-05 03:54:32 -0500

    feat: Defined new methods to read and write user data to database

--- src/backend/database/database-ops.js
+++ src/backend/database/database-ops.js
@@ -1,12 +1,41 @@
-// database-ops module definition
-var database_ops = (function () {
-  // list function definitions here
+// import needed externally defined modules
+import { ref, set, child, get } from 'firebase/database';
 
-  // export public functions
-  return {
-    // list public functions here
-  }
-})();
+// write user data
+export async function writeUserData(db, userId, userName, email, password, firstName, lastName, medInsts, isAdminUser) {
+  const writeUserDataRef = ref(db, `users/${userId}`);
+  await set(writeUserDataRef, {
+    userName: userName,
+    email: email,
+    password: password,
+    firstName: firstName,
+    lastName: lastName,
+    medInsts: medInsts,
+    isAdminUser: isAdminUser
+  })
+  .then(() => {
+    return true;
+  })
+  .catch((_error) => {
+    return false;
+  });
+}
 
-// export the module
-module.exports = database_ops;
+// read user data
+export async function readUserData(db, userId) {
+  return new Promise((resolve, reject) => {
+    const readUserDataRef = ref(db);
+    get(child(readUserDataRef, `users/${userId}`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const userData = snapshot.val();
+          resolve(userData);
+        } else {
+          reject(null);
+        }
+      })
+      .catch((_error) => {
+        reject(null);
+      });
+  });
+}
