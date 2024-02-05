602e9351583a22754118da8f02f0657b29ecb309
Nathaniel Hu <57736234+Nathaniel-Hu@users.noreply.github.com>
2024-02-05 03:58:17 -0500

    feat: Redefined methods to create, delete, sign in and sign out users and read and write user data to database

--- src/backend/database/backend.js
+++ src/backend/database/backend.js
@@ -1,56 +1,83 @@
 // import needed externally defined modules
-const initializeApp = require("firebase/app").initializeApp;
-const getAuth = require("firebase/auth").getAuth;
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 // import needed internally defined modules
-const user_auth_mgmt = require("./user-auth-mgmt");
+import { createNewUser, deleteAUser, signInUser, signOutUser } from './user-auth-mgmt';
+import { readUserData, writeUserData } from './database-ops';
+import { resolve } from "path";
 
-// backend module definition
-var backend = (function () {
-  // necessary firebase configuration setup
-  const firebaseConfig = {
-    apiKey: "AIzaSyB5RHIQl0a8nzZ2bMUwTMJ6XNYWciBaneM",
-    authDomain: "chest-x-ray-ai-f0b4a.firebaseapp.com",
-    databaseURL: "https://chest-x-ray-ai-f0b4a-default-rtdb.firebaseio.com",
-    projectId: "chest-x-ray-ai-f0b4a",
-    storageBucket: "chest-x-ray-ai-f0b4a.appspot.com",
-    messagingSenderId: "106488932425",
-    appId: "1:106488932425:web:74c67977d47848d27f3798",
-    measurementId: "G-085B846RRX",
-  };
+// necessary firebase configuration setip
+const firebaseConfig = {
+  apiKey: "AIzaSyB5RHIQl0a8nzZ2bMUwTMJ6XNYWciBaneM",
+  authDomain: "chest-x-ray-ai-f0b4a.firebaseapp.com",
+  databaseURL: "https://chest-x-ray-ai-f0b4a-default-rtdb.firebaseio.com",
+  projectId: "chest-x-ray-ai-f0b4a",
+  storageBucket: "chest-x-ray-ai-f0b4a.appspot.com",
+  messagingSenderId: "106488932425",
+  appId: "1:106488932425:web:74c67977d47848d27f3798",
+  measurementId: "G-085B846RRX"
+}
 
-  // necessary constants for various firebase functionalities (i.e. backend authentication, databases, etc.)
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
+// necessary constnats for various firebase cuntionalities (i.e. auth, database)
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
 
-  // list function definitions here
+// create a new user
+export function createANewUser(userName, email, password, firstName, lastName, medInsts, isAdminUser) {
+  return new Promise((resolve, reject) => {
+    createNewUser(auth, email, password)
+      .then((userId) => {
+        const userDataWritten = writeUserData(db, userId, userName, email, password, firstName, lastName, medInsts, isAdminUser);
+        if (userDataWritten) {
+          resolve(userId);
+        }
+        else {
+          reject(null);
+        }
+      })
+      .catch((_error) => {
+        reject(null);
+      });
+  });
+}
 
-  // create new user
-  var createNewUser = (email, password) => {
-    user = user_auth_mgmt.createNewUser(auth, email, password);
-    return user.uid;
-  }
+// delete a user
+export async function deleteUser(email, password) {
+  const userDeleted = await deleteAUser(auth, email, password);
+  return userDeleted;
+}
 
-  // delete user
-  var deleteTheUser = (user) => {
-    userDeleted = user_auth_mgmt.deleteTheUser(user);
-    return userDeleted;
-  }
+// sign in a user
+export function signInAUser(email, password) {
+  return new Promise((resolve, reject) => {
+    signInUser(auth, email, password)
+      .then((userId) => {
+        const userData = readUserData(db, userId);
+        if (userData !== null) {
+          resolve(userData);
+        }
+        else {
+          reject(null);
+        }
+      })
+      .catch((_error) => {
+        reject(null);
+      });
+  });
+}
 
-  // sign in user
-  var signInUser = (email, password) => {
-    user = user_auth_mgmt.signInUser(auth, email, password);
-    return user;
-  }
-
-  // export public functions
-  return {
-    // list public functions here
-    createNewUser: createNewUser,
-    deleteTheUser: deleteTheUser,
-    signInUser: signInUser
-  }
-})();
-
-// export the module
-module.exports = backend;
+// sign out a user
+export function signOutAUser() {
+  return new Promise((resolve, reject) => {
+    signOutUser(auth)
+      .then((userSignedOut) => {
+        resolve(userSignedOut);
+      })
+      .catch((_error) => {
+        reject(null);
+      });
+  });
+}
