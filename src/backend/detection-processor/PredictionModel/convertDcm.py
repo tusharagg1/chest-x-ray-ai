92fad5690206008d6dc60b65da1420ffba2a9034
TUSHAR AGGARWAL <AGGARWT@MCMASTER.CA>
2024-03-21 00:52:01 -0400

    fixed code struct

--- src/backend/detection-processor/PredictionModel/convertDcm.py
+++ src/backend/detection-processor/PredictionModel/convertDcm.py
@@ -3,14 +3,16 @@ import pydicom
 import numpy as np
 import os
 
+
 def lin_stretch_img(img, low_prc, high_prc, do_ignore_minmax=True):
-    """ 
-    Apply linear "stretch" - low_prc percentile goes to 0, 
+    """
+    Apply linear "stretch" - low_prc percentile goes to 0,
     and high_prc percentile goes to 255.
-    The result is clipped to [0, 255] and converted to np.uint8
+    The result is clipped to [0, 255] and converted to np.uint8.
 
     Additional feature:
-    When computing high and low percentiles, ignore the minimum and maximum intensities (assumed to be outliers).
+    When computing high and low percentiles, ignore the minimum and maximum
+    intensities (assumed to be outliers).
     """
     # For ignoring the outliers, replace them with the median value
     if do_ignore_minmax:
@@ -21,31 +23,40 @@ def lin_stretch_img(img, low_prc, high_prc, do_ignore_minmax=True):
     else:
         tmp_img = img
 
-    lo, hi = np.percentile(tmp_img, (low_prc, high_prc))  # Example: 1% - Low percentile, 99% - High percentile
+    lo, hi = np.percentile(
+        tmp_img, (low_prc, high_prc)
+    )  # Example: 1% - Low percentile, 99% - High percentile
 
     if lo == hi:
-        return np.full(img.shape, 128, np.uint8)  # Protection: return gray image if lo = hi.
+        return np.full(
+            img.shape, 128, np.uint8
+        )  # Protection: return gray image if lo = hi.
 
-    stretch_img = (img.astype(float) - lo) * (255/(hi-lo))  # Linear stretch: lo goes to 0, hi to 255.
-    stretch_img = stretch_img.clip(0, 255).astype(np.uint8)  # Clip range to [0, 255] and convert to uint8
+    stretch_img = (img.astype(float) - lo) * (
+        255 / (hi - lo)
+    )  # Linear stretch: lo goes to 0, hi to 255.
+    stretch_img = stretch_img.clip(0, 255).astype(
+        np.uint8
+    )  # Clip range to [0, 255] and convert to uint8
     return stretch_img
 
 
 def getimgdata(dcmpath):
 
     ds = pydicom.dcmread(dcmpath)
-    img = ds.pixel_array # get image array
+    img = ds.pixel_array  # get image array
 
-    img = lin_stretch_img(img, 1, 99)  # Apply "linear stretching" (lower percentile 1 goes to 0, and percentile 99 to 255).
+    img = lin_stretch_img(img, 1, 99)  # Apply "linear stretching"
+    # (lower percentile 1 goes to 0, and percentile 99 to 255).
 
     return img
     # TODO: save jpg file in database
-    #cv2.imwrite(target, img)
+    # cv2.imwrite(target, img)
 
 
 # returns filenames in a directory that have given extension
 def getfilenames(directory, extension):
-    
+
     filenames = []
     try:
         for filename in os.listdir(directory):
@@ -53,7 +64,7 @@ def getfilenames(directory, extension):
                 filenames.append(filename)
     except IOError:
         print(f"'{directory}': No such directory")
-    
+
     return filenames
 
 
@@ -66,8 +77,8 @@ def convertdcmtojpg(dcmpath, target):
 # saves a jpg conversion of all dcm files in given dir.
 def convertall(dcmdir):
 
-    dcmfiles = getfilenames(dcmdir, '.dcm')
+    dcmfiles = getfilenames(dcmdir, ".dcm")
     for img in dcmfiles:
         dcmpath = os.path.join(dcmdir, img)
-        target = os.path.join(dcmdir, img[:-3]+'jpg')
-        convertdcmtojpg(dcmpath,target)
+        target = os.path.join(dcmdir, img[:-3] + "jpg")
+        convertdcmtojpg(dcmpath, target)
--- src/backend/detection-processor/PredictionModel/runPredModel.py
+++ src/backend/detection-processor/PredictionModel/runPredModel.py
@@ -3,16 +3,16 @@
 
 import os
 import torch
-import torchvision, torchvision.transforms
+import torchvision.transforms
 import torchxrayvision as xrv
-from PredictionModel.convertDcmtoJpg import getfilenames, getimgdata
+from PredictionModel.convertDcm import getfilenames, getimgdata
 
 
 # get predictions from a dcm x-ray
 def getprediction(img_path, weights="densenet121-res224-all"):
 
     img = getimgdata(img_path)
-    img = xrv.datasets.normalize(img, 255)  
+    img = xrv.datasets.normalize(img, 255)
 
     # Check that images are 2D arrays
     if len(img.shape) > 2:
@@ -23,7 +23,6 @@ def getprediction(img_path, weights="densenet121-res224-all"):
     # Add color channel
     img = img[None, :, :]
 
-    
     # the models will resize the input to the correct size so this is optional.
     transform = torchvision.transforms.Compose([xrv.datasets.XRayCenterCrop()])
 
@@ -34,43 +33,51 @@ def getprediction(img_path, weights="densenet121-res224-all"):
     with torch.no_grad():
         img = torch.from_numpy(img).unsqueeze(0)
         preds = model(img).cpu()
-        output["preds"] = dict(zip(xrv.datasets.default_pathologies,preds[0].detach().numpy()))
+        output["preds"] = dict(
+            zip(xrv.datasets.default_pathologies, preds[0].detach().numpy())
+        )
 
-    diseases = ['Atelectasis', 'Pneumonia', 'Cardiomegaly', 'Pleural_Thickening']
+    diseases = [
+        "Atelectasis",
+        "Pneumonia",
+        "Cardiomegaly",
+        "Pleural_Thickening",
+    ]
     allpreds = output.get("preds")
     predictions = {}
     if allpreds:
         for disease in diseases:
             predictions[disease] = allpreds.get(disease)
-    else :
+    else:
         print("results not found")
         return {}
-    
+
     return allpreds
 
 
 # returns combined predictions for all dcm xrays in a directory
 def scanallxrays(xraydir):
 
-    #convertall(xraydir)
+    # convertall(xraydir)
 
-    xray_imgs = getfilenames(xraydir,'.dcm')
+    xray_imgs = getfilenames(xraydir, ".dcm")
     allpreds = []
 
     for img in xray_imgs:
         preds = getprediction(os.path.join(xraydir, img))
-        #print(os.path.join(xraydir, img))
+        # print(os.path.join(xraydir, img))
         allpreds.append(preds)
-    
 
     if len(allpreds) > 1:
-        
+
         finalpreds = {}
         pnum = len(allpreds)
         diseases = allpreds[0].keys()
         for disease in diseases:
             # taking avg value of predictions for all diseases
-            combined_preds = sum([allpreds[i][disease] for i in range(pnum)])/pnum
+            combined_preds = (
+                sum([allpreds[i][disease] for i in range(pnum)]) / pnum
+            )
             finalpreds[disease] = combined_preds
         return finalpreds
     else:
