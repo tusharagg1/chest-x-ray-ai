df5ac2e89c2727e31c9c3415a950481afb1a07c3
Mohaansh Pranjal <58990171+mohaanshpranjal@users.noreply.github.com>
2024-04-04 23:04:44 -0400

    fix: ESLint errors

--- src/frontend/src/app/PatientSearch/page.tsx
+++ src/frontend/src/app/PatientSearch/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+//ts comments stop errors that didn't effect compliation or function
 /*
 * Author: Allison Cook
 * Date Created: January 2024
